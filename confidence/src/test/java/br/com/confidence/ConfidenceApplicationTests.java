c16f2b76b3dcd62863d486e3c6013de07fd5a675
YarleiWillersDev <yarlei.willers@gmail.com>
2025-11-25 09:57:45 -0300

    Initial project structure and setup

---
+++ confidence/src/test/java/br/com/confidence/ConfidenceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.confidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
