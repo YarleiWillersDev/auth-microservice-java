3a783d68c598d4da3fe7281a8a037a4434865577
YarleiWillersDev <yarlei.willers@gmail.com>
2025-12-15 19:09:08 -0300

    feat(security): setup stateless JWT security with public auth endpoints and ADMIN-only user/role management

---
+++ confidence/src/main/java/br/com/confidence/security/AuthorizationService.java
@@ -0,0 +1,26 @@
+package br.com.confidence.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.confidence.exception.user.UserNotFoundException;
+import br.com.confidence.repository.user.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+}
